04c0ec9f1313507a0cd0a4e240dcdbb144a780d3
Offliners <eric21904@gmail.com>
2021-08-08 15:06:52 +0800

    hw2

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -6,8 +6,6 @@
 using namespace std;
 using namespace cv;
 
-Mat drawHist(const Mat&);
-
 int main(int argc, char **argv)
 {
     if(argc != 2)
@@ -31,7 +29,18 @@ int main(int argc, char **argv)
     cout << "Downloaded binarize image" << endl;
 
     // histogram
-    Mat img2 = drawHist(img);
+    Mat hist;
+    int histSize = 256;
+    float range[] = {0, 255};
+    const float* histRange = {range};
+    calcHist(&img, 1, 0, Mat(), hist, 1, &histSize, &histRange);
+    Mat img2(256, 256, CV_8UC1, Scalar(255));
+    float histmax = *max_element(hist.begin<float>(), hist.end<float>());
+    for(int i = 0; i < 256; ++i)
+    {
+        int intensity = static_cast<int>(hist.at<float>(i) * 0.9 * 256 / histmax);
+        line(img2, Point(i, 255), Point(i, 255 - intensity), Scalar(0));
+    }
     imwrite("demo/histogram.png", img2);
     cout << "Downloaded histogram image" << endl;
 
@@ -58,25 +67,4 @@ int main(int argc, char **argv)
     cout << "Downloaded connected-components image" << endl;
 
     return 0;
-}
-
-Mat drawHist(const Mat& image)
-{
-    Mat hist;
-    int histSize = 256;
-    float range[] = {0, 255};
-    const float* histRange = {range};
-
-    calcHist(&image, 1, 0, Mat(), hist, 1, &histSize, &histRange);
-    Mat output(256, 256, CV_8UC1, Scalar(255));
-    
-    float histmax = *max_element(hist.begin<float>(), hist.end<float>());
-
-    for(int i = 0; i < 256; ++i)
-    {
-        int intensity = static_cast<int>(hist.at<float>(i) * 0.9 * 256 / histmax);
-        line(output, Point(i, 255), Point(i, 255 - intensity), Scalar(0));
-    }
-
-    return output;
 }
