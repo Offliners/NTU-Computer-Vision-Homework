1c07e9e09b20cb9aeb916e446c9483a9d6553973
Offliners <eric21904@gmail.com>
2021-08-08 14:47:17 +0800

    hw2

--- hw2/hw2.cpp
+++ hw2/hw2.cpp
@@ -35,6 +35,28 @@ int main(int argc, char **argv)
     imwrite("demo/histogram.png", img2);
     cout << "Downloaded histogram image" << endl;
 
+    // connected components
+    Mat labels, stats, centroids, img3;
+    cvtColor(img1, img3, COLOR_GRAY2RGB);
+    connectedComponentsWithStats(img1, labels, stats, centroids, 8);
+    for(int i = 0; i < stats.rows; ++i)
+    {
+        int x = stats.at<int>(Point(0, i));
+        int y = stats.at<int>(Point(1, i));
+        int width = stats.at<int>(Point(2, i));
+        int height = stats.at<int>(Point(3, i));
+        int area = stats.at<int>(Point(4, i));
+
+        if(area < 500 || area > 120000)
+            continue;
+
+        drawMarker(img3, Point(x + width / 2, y + height / 2), Scalar(0, 0, 255), MARKER_CROSS, 20, 2);
+        rectangle(img3, Rect(x, y, width, height), Scalar(255, 0, 0), 3);
+    }
+
+    imwrite("demo/connected-components.png", img3);
+    cout << "Downloaded connected-components image" << endl;
+
     return 0;
 }
 
