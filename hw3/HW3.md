f681124dab8c0d7198c82e61b9eb149ad51f7805
Offliners <r10522801@ntu.edu.tw>
2021-08-17 22:06:44 +0800

    hw3

--- hw3/HW3.md
+++ hw3/HW3.md
@@ -18,8 +18,18 @@ Write a program to generate images and histograms:
 
 (a) original image and its histogram
 
-![histogram](demo/histogram.png)
+|Image|histogram|
+|-|-|
+|![origin](demo/origin.png)|![histogram](demo/histogram.png)|
 
 (b) image with intensity divided by 3 and its histogram
 
+|Image|histogram|
+|-|-|
+|![origin_divide_3](demo/origin_divide_3.png)|![histogram_divide_3](demo/histogram_divide_3.png)|
+
 (c) image after applying histogram equalization to (b) and its histogram
+
+|Image|histogram|
+|-|-|
+|![histogram_equalization_image](demo/histogram_equalization_image.png)|![histogram_equalization_histogram](demo/histogram_equalization_histogram.png)|
--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -22,6 +22,9 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    imwrite("demo/origin.png", img);
+    cout << "Downloaded origin image" << endl;
+
     // histogram
     Mat hist;
     int histSize = 256;
@@ -37,6 +40,39 @@ int main(int argc, char **argv)
     }
     imwrite("demo/histogram.png", img1);
     cout << "Downloaded histogram image" << endl;
+
+    // divided by 3
+    Mat img2(img / 3.);
+    imwrite("demo/origin_divide_3.png", img2);
+    cout << "Downloaded origin_divide_3 image" << endl;
+
+    calcHist(&img2, 1, 0, Mat(), hist, 1, &histSize, &histRange);
+    Mat img3(256, 256, CV_8UC1, Scalar(255));
+    histmax = *max_element(hist.begin<float>(), hist.end<float>());
+    for(int i = 0; i < 256; ++i)
+    {
+        int intensity = static_cast<int>(hist.at<float>(i) * 0.9 * 256 / histmax);
+        line(img3, Point(i, 255), Point(i, 255 - intensity), Scalar(0));
+    }
+    imwrite("demo/histogram_divide_3.png", img3);
+    cout << "Downloaded histogram_divide_3 image" << endl;
+
+    // histogram equalization
+    Mat img4(img.size(), CV_8UC1);
+    equalizeHist(img2, img4);
+    imwrite("demo/histogram_equalization_image.png", img4);
+    cout << "Downloaded histogram_equalization_image image" << endl;
+
+    calcHist(&img4, 1, 0, Mat(), hist, 1, &histSize, &histRange);
+    Mat img5(256, 256, CV_8UC1, Scalar(255));
+    histmax = *max_element(hist.begin<float>(), hist.end<float>());
+    for(int i = 0; i < 256; ++i)
+    {
+        int intensity = static_cast<int>(hist.at<float>(i) * 0.9 * 256 / histmax);
+        line(img5, Point(i, 255), Point(i, 255 - intensity), Scalar(0));
+    }
+    imwrite("demo/histogram_equalization_histogram.png", img5);
+    cout << "Downloaded histogram_equalization_histogram image" << endl;
 
     return 0;
 }
